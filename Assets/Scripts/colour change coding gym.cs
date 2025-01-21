b97136b3ede952cb1fc078ebf1c1ea3502cb4fe6
Julia Versaevel <versaeve@shernet.sheridancollege.ca>
2025-01-21 17:42:22 -0500

    made it so that sprite starts out white and changes to red when moue clicked

---
+++ Assets/Scripts/colour change coding gym.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colourchangecodinggym : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    //public Color white;
+   // public Color red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     spriteRenderer.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // if (Input.GetMouseButtonDown(0)) - change colour from white to red when clicked
+        {
+
+            // Color Update();
+           // spriteRenderer.color = Color.red;
+        }
+
+        
+        
+
+    }
+}
